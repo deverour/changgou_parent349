ef3e5b2603e3128cfe9b6aa54665e71213bd4903
deverour <361844795@qq.com>
2020-06-23 16:51:45 +0800

    v10.10

--- changgou_service/changgou_service_search/src/main/java/com/changgou/search/controller/SearchController.java
+++ changgou_service/changgou_service_search/src/main/java/com/changgou/search/controller/SearchController.java
@@ -1,15 +1,19 @@
 package com.changgou.search.controller;
 
+import com.changgou.entity.Page;
+import com.changgou.search.pojo.SkuInfo;
 import com.changgou.search.service.EsManagerService;
 import com.changgou.search.service.SearchService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 
 import java.util.Map;
 import java.util.Set;
 
-@RestController
+@Controller
 @RequestMapping("/search")
 public class SearchController {
 
@@ -25,7 +29,7 @@ public class SearchController {
      * @param searchMap
      * @return
      */
-    public void paramHandler(Map<String,String> searchMap){
+    private void paramHandler(Map<String,String> searchMap){
         if (searchMap !=null && searchMap.size() > 0){
             for (String key : searchMap.keySet()) {
                 if (key.startsWith("spec_")){
@@ -36,14 +40,53 @@ public class SearchController {
         }
     }
 
-
-
-
+    /**
+     * 高级搜索
+     * @param searchMap
+     * @return
+     */
     @GetMapping
+    @ResponseBody
     public Map search(@RequestParam Map<String,String> searchMap){
         //特殊符号处理
         paramHandler(searchMap);
         Map resultMap = searchService.search(searchMap);
         return resultMap;
+    }
+
+    /**
+     * 搜索并返回到搜索结果列表页面
+     * @param searchMap
+     * @return
+     */
+    @GetMapping("/list")
+    public String list(@RequestParam Map<String,String> searchMap, Model model){
+        paramHandler(searchMap);
+        //搜索并返回结果数据
+        Map resultMap = searchService.search(searchMap);
+        model.addAttribute("result",resultMap);
+        model.addAttribute("searchMap",searchMap);
+
+        Page<SkuInfo> page = new Page<SkuInfo>(
+                Long.parseLong(String.valueOf(resultMap.get("total"))) ,
+                Integer.parseInt(String.valueOf(resultMap.get("pageNum"))) ,
+                Page.pageSize);
+
+        StringBuilder url = new StringBuilder("/search/list");
+        if (searchMap != null && searchMap.size()>0){
+            url.append("?");
+            for (String paramKey : searchMap.keySet()) {
+                if (!"sortRule".equals(paramKey) && !"sortField".equals(paramKey) && !"pageNum".equals(paramKey) ){
+                    url.append(paramKey).append("=").append(searchMap.get(paramKey)).append("&");
+                }
+            }
+            String urlStr = url.toString();
+            urlStr = url.substring(0, url.length() - 1);
+            model.addAttribute("url",urlStr);
+        }else {
+            model.addAttribute("url",url);
+        }
+
+        return "search";
     }
 }
